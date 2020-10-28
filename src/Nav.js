9de9ec780b905e0eeddf43d3053a68b46b79e8a3
RavJain007 <ravjain007@gmail.com>
2020-10-28 16:25:39 +0530

    Netflix

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=41"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_side"
+        src="https://pbs.twimg.com/media/DlKNEufWsAAgr2E.jpg"
+        alt="nav_smiley"
+      />
+    </div>
+  );
+}
+
+export default Nav;
